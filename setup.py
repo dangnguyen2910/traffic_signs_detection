c36854f538de91193e7dd09f75fc03d35884905a
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2025-01-07 21:14:21 +0700

    ADD: .gitignore, setup data and folders

---
+++ setup.py
@@ -0,0 +1,23 @@
+import subprocess 
+import zipfile
+import os
+import shutil
+
+zip_file = 'cardetection.zip'
+
+
+if not os.path.exists("data"):
+    subprocess.run("curl -L -o cardetection.zip\
+      https://www.kaggle.com/api/v1/datasets/download/pkdarabi/cardetection", shell=True, check=True)
+
+    with zipfile.ZipFile(zip_file, 'r') as f:
+        f.extractall()
+
+    os.remove(zip_file)
+    os.rename('car', 'data')
+    shutil.move('video.mp4', 'data/video.mp4')
+
+if not os.path.exists("figures"):
+    os.makedirs("figures")
+
+
