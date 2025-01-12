cf6ea6e95e98fbdee01bf1613d4e6f8cc446a7ec
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2025-01-12 21:31:39 +0700

    Add: Train and evaluate model

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 from ultralytics import YOLO
+import os 
 
-model = YOLO("yolo11n.pt")
 
-train_result = model.train(data = "../data/data.yaml", epochs = 2, imgsz = 416, device = 0)
+def main(): 
+    epochs = 2
+    batch_size = 8
+    device = 0
+
+    lr_start = 0.001
+    lr_end = 0.0001
+
+    version = 'v1.0'
+
+    # Train
+    want_train = input("Train (y/n)? ")
+    if want_train == 'y':
+        model = YOLO("yolo11n.pt")
+        train_result = model.train(data = "data/data.yaml", epochs = epochs, 
+                                   imgsz = 416, batch = 8, device = 0, cos_lr = True, lr0 = 0.001, lrf = 0.0001, 
+                                   project = 'train_result', name=version)
+        return 1
+
+    model = YOLO("runs/detect/" + version + "best.pt")
+    metrics = model.val(data = "data/data.yaml", split = "test")
+    print(f"mAP@0.5: {metrics.box.map50:.4f}")
+    print(f"mAP@0.5:0.95: {metrics.box.map:.4f}")
+    print(f"Precision: {metrics.box.p:.4f}")
+    print(f"Recall: {metrics.box.r:.4f}")
+    print(f"F1-Score: {metrics.box.f1:.4f}")
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
